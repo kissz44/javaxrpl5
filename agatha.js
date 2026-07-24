1ba02404d831d15f43699c62775ea2281e811f51
kissz44 <agathazakies18@gmail.com>
2026-07-24 08:10:06 +0700

    Add files via upload

---
+++ agatha.js
@@ -0,0 +1,4 @@
+console.log("nama               : Agatha Zakies Geovany Junior");
+console.log("tanggal lahir      : 18 Juni 2010");
+console.log("SMP                : SMPn 4 KEPANJEN");
+console.log("alasan masukan rpl : melanjutkan materi yang sudah di dapat di SMP");

87809f10f8c45f4adac2d638b999307e3336cd28
kissz44 <agathazakies18@gmail.com>
2026-07-31 08:38:23 +0700

    Add files via upload

---
+++ karyawan.js
@@ -0,0 +1,13 @@
+const karyawan = {
+    nama : "agatha zakies", //string
+    umur : 16, //number
+    gaji: 12000000, //number
+    status : "true", //boolean
+    jabatan : "null", //null
+};
+//template literal
+console.log(`nama    : ${karyawan.nama}`)
+console.log(`umur    : ${karyawan.umur}`)
+console.log(`gaji    : ${karyawan.gaji}`)
+console.log(`status  : ${karyawan.status}`)
+console.log(`jabatan : ${karyawan.jabatan}`)
